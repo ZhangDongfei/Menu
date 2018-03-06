bf25d2e317548ff171f9298b55b8f2ef02b1e760
zhangdongfei <2254243869@qq.com>
2018-03-06 20:33:41 +0800

    first commit

---
+++ ForCreateTemplate/WebService/Controllers/ConfigController.cs
@@ -0,0 +1,31 @@
+﻿using ForCreateTemplate.WebService.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ForCreateTemplate.WebService.Controllers
+{
+    public class ConfigController : ApiController
+    {
+        /// <summary>
+        /// 获取应用信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public AppInfo Index()
+        {
+            return new AppInfo
+            {
+                Status = 1,
+                Result = new Data
+                {
+                    Copyright = "Codeveloper Org.",
+                    Name = "模板应用",
+                    Version = "0.1"
+                }
+            };
+        }
+    }
+}
---
+++ ForCreateTemplate/WebService/Models/Response/AppInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace ForCreateTemplate.WebService.Models.Response
+{
+    public class AppInfo
+    {
+        public int Status { get; set; }
+
+        public Data Result { get; set; }
+    }
+
+    public class Data
+    {
+        /// <summary>
+        /// 应用名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 应用版本
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// 版权归属
+        /// </summary>
+        public string Copyright { get; set; }
+    }
+}
